7754562f2e94691f917d37dd135050253de296d7
Deepak <dgusaiwal537@gmail.com>
2023-10-21 17:50:39 +0530

    improve the code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let nextBtn = document.querySelector('.next')
 let prevBtn = document.querySelector('.prev')
 let elems = [...document.querySelectorAll('.elem')];
+let imgElems = [...document.querySelectorAll('.slider .imgElem')];
 
 
 elems.forEach((elem) => {
@@ -16,93 +17,83 @@ elems.forEach((elem) => {
                 duration: 1,
                 onComplete: function () {
                     gsap.set(this._targets[0], { top: "100%" })
-                    isAnimating = false;
                 }
             });
-            i < h1s.length - 1 ? i++ : i = 0
+            gsap.to(imgElems[i], {
+                width: 0,
+                duration: 1,
+                left: "100%",
+                ease: Expo.easeInOut,
+                onComplete: function () {
+                    gsap.set(this._targets[0], { left: "0%", opacity: 0, width: "40%" });
+                    isAnimating = false;
+                }
+            })
+
+            i < h1s.length - 1 ? i++ : i = 0  // can use h1s/imgElems length
+
             gsap.to(h1s[i], {
                 top: "-=100%",
                 ease: Expo.easeInOut,
                 duration: 1
             });
 
+            gsap.to(imgElems[i], {
+                width: "100%",
+                duration: 1,
+                left: "50%",
+                opacity: 1,
+                ease: Expo.easeInOut,
+            })
+
         }
     })
 
+
     prevBtn.addEventListener('click', () => {
         if (!isAnimating) {
+            isAnimating = true;
             gsap.to(h1s[i], {
                 top: "-=100%",
                 ease: Expo.easeInOut,
                 duration: 1,
                 onComplete: function () {
                     gsap.set(this._targets[0], { top: "100%" });
-                    isAnimating = false;
                 }
             });
-            i > 0 ? i-- : i = h1s.length - 1;
+            gsap.to(imgElems[i], {
+                width: 0,
+                duration: 1,
+                left: "100%",
+                ease: Expo.easeInOut,
+                onComplete: function () {
+                    gsap.set(this._targets[0], { left: "0%", opacity: 0, width: "40%" });
+                    isAnimating = false;
+                }
+            })
+
+            i > 0 ? i-- : i = h1s.length - 1; // can use h1s/imgElems length
+
             gsap.to(h1s[i], {
                 top: "-=100%",
                 ease: Expo.easeInOut,
                 duration: 1,
             });
+
+            gsap.to(imgElems[i], {
+                width: "100%",
+                duration: 1,
+                left: "50%",
+                opacity: 1,
+                ease: Expo.easeInOut,
+            })
+
         }
+
     })
 })
 
-// for images
-{
-    let imgElems = [...document.querySelectorAll('.slider .imgElem')];
-    let isAnimating = false;
-    let i = 0;
-    nextBtn.addEventListener('click', () => {
-        if (!isAnimating) {
-            isAnimating = true;
-            gsap.to(imgElems[i], {
-                width: 0,
-                duration: 1,
-                left: "100%",
-                ease: Expo.easeInOut,
-                onComplete: function () {
-                    gsap.set(this._targets[0], { left: "0%", opacity: 0, width: "40%" });
-                    isAnimating = false;
-                }
-            })
-            i < imgElems.length - 1 ? i++ : i = 0;
-            gsap.to(imgElems[i], {
-                width: "100%",
-                duration: 1,
-                left: "50%",
-                opacity: 1,
-                ease: Expo.easeInOut,
-            })
-        }
-
-    })
-    prevBtn.addEventListener('click', () => {
-        if (!isAnimating) {
-            isAnimating = true;
-            gsap.to(imgElems[i], {
-                width: 0,
-                duration: 1,
-                left: "100%",
-                ease: Expo.easeInOut,
-                onComplete: function () {
-                    gsap.set(this._targets[0], { left: "0%", opacity: 0, width: "40%" });
-                    isAnimating = false;
-                }
-            })
-            i > 0 ? i-- : i = imgElems.length - 1;
-            gsap.to(imgElems[i], {
-                width: "100%",
-                duration: 1,
-                left: "50%",
-                opacity: 1,
-                ease: Expo.easeInOut,
-            })
-        }
-
-    })
-
-}
+setInterval(() => {
+    nextBtn.click();
+}, 3000);
 
